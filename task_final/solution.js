8fd1d627a776838c9b5c9585e4e6033a9cbee9ab
Tsomkal Marina <marinats000@yandex.ru>
2022-01-03 21:54:06 +0300

    done 2

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,26 +33,21 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum, id, price, title, product, street, house, entrance, floor, flat) {
-    count = goods.count;
-    
-    let data = {client: (name, phone),order: {address: 'ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53',sum:900}, goods: [{title:'Пицца', count:2}], product };
+function sendRequest(name, phone, address, goods, sum) {
+   
+  
+    let data = {client: {name, phone},order: {address, sum}, goods: [{title, count}]};
     let countOfGoods = data.goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i]); 
+    let product;
+    product = {length: [{title, count}]};
+    for (let i = 0; i < countOfGoods; i++) {
+       data.goods.push(goods[i].length); 
     }
     
-    data.product = id,price;
+    data.order.address = `${street}` , `${house}`, `${entrance}`, `${floor}`, `${flat}`;
+    data.client = `${name}` , `${' '}` , `${phone}`;
 
-    data.order.address =`${street, house, entrance, floor, flat}`;
     data.order.sum = sum;
-
-    data.client = `${name + ' ' + phone}`;
-
-    data.goods.title = title;
-    data.goods = [{id, title, count, price}];
-
     let jsonData = JSON.stringify({data});
 
     return jsonData;
